2dfeb91277211f5dae5efb391ac2c9faffd8c17f
ndukwon <ndukwon@gmail.com>
2016-11-18 17:17:34 +0900

    [main] print "Hello world!!"

--- src/com/duk/lab/Main.java
+++ src/com/duk/lab/Main.java
@@ -3,8 +3,6 @@ package com.duk.lab;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.out.println("Hello world!!");
     }
-
 }
