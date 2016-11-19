36517671d2e21e6a1d522a6e04095871eb3054a0
ndukwon <ndukwon@gmail.com>
2016-11-19 13:48:31 +0900

    [InsertSort] add library

---
+++ src/com/duk/lab/common/Item.java
@@ -0,0 +1,13 @@
+package com.duk.lab.common;
+
+public class Item implements Comparable<Item> {
+    private final int mKey;
+    public Item(int key) {
+        mKey = key;
+    }
+
+    @Override
+    public int compareTo(Item itemToCompare) {
+        
+    }
+}
